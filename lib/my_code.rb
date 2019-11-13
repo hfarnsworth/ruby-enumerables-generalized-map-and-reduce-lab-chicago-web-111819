678d514abe1f415fef923665576093291ea44d7e
Havilah J Farnsworth <havilahfarnsworth@gmail.com>
2019-11-13 11:54:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,33 @@
 # Your Code Here
+def map(array)
+  index = 0 
+  newArray = []
+  
+  while index < array.length do 
+    transform = yield(array[index])
+    newArray << transform
+    index += 1 
+  end
+    
+    newArray
+end
+
+def reduce(array, starting_point = 0)
+  
+  index = 0 
+  n = starting_point
+  while index < array.length do 
+    memo = array[index]
+    if memo
+      if memo.is_a? Integer 
+        n += memo
+      else
+      end
+    else 
+      return false
+    end
+    index += 1 
+  end
+  
+  n
+end
